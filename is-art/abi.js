8332d282028b37e2ec724a4eb5d2406302098e3c
Yvonne213 <hywener@gmail.com>
2022-09-27 09:33:54 -0500

    Update abi.js

--- is-art/abi.js
+++ is-art/abi.js
@@ -1,5 +1,39 @@
 // change this each time you deploy a new contract
-var contractAddress = 
+var contractAddress = '0x34191f84b62ED43FE8407Af4bed4d591Eba5B307';
 
 // paste contract ABI here
-var contractABI = 
+var contractABI = [
+	{
+		"inputs": [],
+		"name": "changeStatus",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "getStatus",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "isArt",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
