b8515d9e1b9d4d1fa1890b0414d3f914ab6f95c6
Yvonne213 <hywener@gmail.com>
2022-09-27 09:50:55 -0500

    Update abi.js

--- is-art-democratic-public/abi.js
+++ is-art-democratic-public/abi.js
@@ -1,18 +1,19 @@
 // change this each time you deploy a new contract
-var contractAddress = "";
+var contractAddress = "0xcbE0ca0461cF47386Ee181a999C805e4cC1487EF";
 
 // paste contract ABI here
 var contractABI = [
 	{
 		"inputs": [],
-		"name": "getStatus",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
+		"name": "voteNo",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "voteYes",
+		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
@@ -44,16 +45,33 @@ var contractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "voteNo",
-		"outputs": [],
-		"stateMutability": "nonpayable",
+		"name": "viewStatus",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
 		"inputs": [],
-		"name": "voteYes",
-		"outputs": [],
-		"stateMutability": "nonpayable",
+		"name": "viewVotes",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
