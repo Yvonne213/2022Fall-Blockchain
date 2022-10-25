0e30169c9d1636a88e7ba159c58a20e7092db41f
Yvonne213 <hywener@gmail.com>
2022-10-25 13:19:10 -0500

    text-NFT

---
+++ nft-text/nft-text/main.js
@@ -0,0 +1,48 @@
+connectButton.onclick = main;
+
+async function main() {
+  if(navigator.userAgent.indexOf("Safari") > -1) {
+    alert("Please switch to a browser that supports Web3 (Chrome, Firefox, Brave, Edge, or Opera)")
+    return;
+  }
+  if(!window.ethereum) {
+    alert("No Web3 Provider detected, please install MetaMask (https://metamask.io)")
+    return;
+  }
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  const chainId = await provider.getNetwork();
+  if(chainId.chainId != 5) {
+    alert("Please switch to the Goerli Test Network.");
+    return;
+  }
+  
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, contractABI, provider);
+  const contractWithSigner = contract.connect(signer);
+
+  // if network changes, refresh the page
+  provider.on("network", (newNetwork, oldNetwork) => {
+    if (oldNetwork) {
+        window.location.reload();
+    }
+  });
+
+  //-----------ADD YOUR CODE BELOW THIS LINE------------//
+
+
+
+  // EVENT LISTENERS
+  contract.on("MintEvent", (_tokenId, _text) => {
+    textDisplay.textContent = _text;
+  });
+ 
+  textInputButton.onclick = mint();
+
+
+  async function mint() {
+    const newText = textInput.value;
+    contractWithSigner.safeMint(newText);
+  }
+}
