e62dba039eb53e4478267ba4720a6d972b6962ce
Yvonne213 <hywener@gmail.com>
2022-11-13 18:36:00 -0600

    add p5js sketch

--- InCircleArtist/main.js
+++ InCircleArtist/main.js
@@ -1,3 +1,17 @@
+/* CONNECT_AUTOMATICALLY
+  true: automatically connect to Web3 Provider on page load.
+  false: enable "click to connect" button
+*/
+// const CONNECT_AUTOMATICALLY = false;
+
+// if(CONNECT_AUTOMATICALLY) {
+//   main();
+// } else {
+//   connectButton.onclick = main;
+// }
+
+
+
 // MetaMask is our 'provider' in this case
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 
@@ -81,6 +95,12 @@ async function getUserInfo() {
   console.log(nameAndAddressArray)
   //document.getElementById("currentArtist").innerHTML = "<tr>"+"jjsjsjs"+"</tr>"
   // $('#currentAddress').text(currentAddress)
+
+
+  //---------------P5.JS----------------------------------//
+  // pretend this is where setup() starts
+  // let c = createCanvas(windowWidth, windowHeight);
+  // c.parent("container")
 }
 
 
@@ -98,5 +118,70 @@ function setInfo() {
     contractWithSigner.nameInput(addressToSet, nameToSet);
   }
 
+}
+
+
+//---------p5js---------------//
+
+// GLOBAL VARIABLES
+let noiseMax=2
+let zoff = 0 
+var xoff1=0
+var xoff2=10000
+
+// this delays the draw loop from starting on page load
+let started = false;
+setTimeout(function(){
+  started = true;
+}, 1000);
+
+
+function setup() {
+  let c = createCanvas(windowWidth, windowHeight);
+  c.parent("container")
+}
+
+
+// this is basically the regular p5.js draw() function
+//add " async " //
+async function draw() {
+  background(255);
+
+    push();
+  translate(width/2, height/2)
+  stroke("red")
+  strokeWeight(4)
+  noFill()
+  beginShape()
+  for (let a=0;a < TWO_PI; a+=0.01){
+    let xoff = map(cos(a), -1,1,0,noiseMax)
+    let yoff = map(sin(a), -1,1,0,noiseMax)
+    let r = map(noise(xoff,yoff,zoff),0,1,350,450)
+    let x = r*cos(a)
+    let y = r*sin(a)
+    vertex(x,y)
+  }
+  endShape(CLOSE)
+  zoff+= 0.01
+   pop();
+
+   push();
+  var x= map(noise(xoff1), 0, 1,0, width);
+  var y= map(noise(xoff2), 0, 1, 0, height);
+
+  xoff1 +=0.005
+  xoff2 +=0.005
+  fill("red")
+  strokeWeight(3)
+  stroke("#F8968F")
+textSize(20);
+text('ARTIST', x, y);
+
+  pop();
+  if (mouseIsPressed === true) {
+   noiseMax =10
+  } else {
+    noiseMax=2
+  }
 }
 
