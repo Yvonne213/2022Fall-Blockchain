6aa2b44f5c8d60f88d92f23c5add0ac0043cbffc
Yvonne213 <hywener@gmail.com>
2022-11-21 19:26:14 -0600

    nft no.1

--- C-gif-framesave/sketch.js
+++ C-gif-framesave/sketch.js
@@ -9,14 +9,14 @@ let easing = 0.08;
 
 
 P5Capture.setDefaultOptions({
-  format: "jpg",
+  format: "gif",
   quality: 0.92,
   disableScaling: true,
   autoSaveDuration: 30,
 })
 
 function setup() {
-  p5Canvas = createCanvas(500, 500);
+  p5Canvas = createCanvas(1000, 1000);
   chasingPoint = createVector(0, 0);
   frameRate(30);
 }
@@ -25,23 +25,23 @@ function draw() {
   if (frameCount ===1) {
     const capture = P5Capture.getInstance();
     capture.start({
-      format: "jpg",
+      format: "gif",
     
-      duration: 30,
+      duration: 300,
     });
   }
 
   background(255);
   push();
   translate(width / 2, height / 2)
-  stroke(255, 0, 0, 50)
+  stroke(255, 0, 0, 100)
   strokeWeight(4)
   noFill()
   beginShape()
   for (let a = 0; a < TWO_PI; a += 0.01) {
     let xoff = map(cos(a), -1, 1, 0, noiseMax)
     let yoff = map(sin(a), -1, 1, 0, noiseMax)
-    let r = map(noise(xoff, yoff, zoff), 0, 1, width / 4, width / 2.5)
+    let r = map(noise(xoff, yoff, zoff), 0, 1, width /3, width / 2.5)
 
     let x = r * cos(a)
     let y = r * sin(a)
@@ -64,32 +64,32 @@ function draw() {
   }
   endShape(CLOSE)
 
-  //   beginShape()
-  //   for (let a=0;a < TWO_PI; a+=0.01){
-  //     let xoff = map(cos(a), -1,1,0,noiseMax)
-  //     let yoff = map(sin(a), -1,1,0,noiseMax)
+    // beginShape()
+    // for (let a=0;a < TWO_PI; a+=0.01){
+    //   let xoff = map(cos(a), -1,1,0,noiseMax)
+    //   let yoff = map(sin(a), -1,1,0,noiseMax)
 
-  //        let r2 = map(noise(xoff,yoff,zoff),0,1,width/4.5,width/5)
-  //        let x2 = r2*cos(a)
-  //     let y2 = r2*sin(a)
-  //      vertex(x2,y2-200)
+    //      let r2 = map(noise(xoff,yoff,zoff),0,1,width/4.5,width/5)
+    //      let x2 = r2*cos(a)
+    //   let y2 = r2*sin(a)
+    //    vertex(x2,y2-200)
 
-  //   }
-  //   endShape(CLOSE)
-  //    beginShape()
-  //   for (let a=0;a < TWO_PI; a+=0.01){
-  //     let xoff = map(cos(a), -1,1,0,noiseMax)
-  //     let yoff = map(sin(a), -1,1,0,noiseMax)
+    // }
+    // endShape(CLOSE)
+    //  beginShape()
+    // for (let a=0;a < TWO_PI; a+=0.01){
+    //   let xoff = map(cos(a), -1,1,0,noiseMax)
+    //   let yoff = map(sin(a), -1,1,0,noiseMax)
 
-  //        let r2 = map(noise(xoff,yoff,zoff),0,1,width/4.5,width/5)
-  //        let x2 = r2*cos(a)
-  //     let y2 = r2*sin(a)
-  //      vertex(x2,y2+200)
+    //      let r2 = map(noise(xoff,yoff,zoff),0,1,width/4.5,width/5)
+    //      let x2 = r2*cos(a)
+    //   let y2 = r2*sin(a)
+    //    vertex(x2,y2+200)
 
-  //   }
-  //   endShape(CLOSE)
+    // }
+    // endShape(CLOSE)
 
-  zoff += 0.01
+  zoff += 0.05
   pop();
 
   //    push();
@@ -106,8 +106,27 @@ function draw() {
   if (mouseIsPressed === true) {
     noiseMax = 10
   } else {
-    noiseMax = 2
+    noiseMax = 4
   }
+
+  push();
+textSize(25);
+fill(255,0,0,150)
+text('Welcome to the circle!', width/2, 920);
+
+pop();
+
+textAlign(CENTER, CENTER);
+push();
+textSize(45);
+text('The person who stands ', width/2, height/2.5+50);
+text('in the circle is an artist.', width/2, height/2.1+50);
+textSize(20);
+textStyle(ITALIC);
+text('--Artist Manifesto', width/2, height/1.8+50);
+fill(0, 0, 0);
+pop();
+  
 
 //   capturer.capture(p5Canvas.canvas);
 //   if (frameCount === 32) {
